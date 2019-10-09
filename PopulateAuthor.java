f64b01a531fec7648e2095ac9abbe6609120fc5d
gfasil <54653776+gfasil@users.noreply.github.com>
2019-10-09 12:33:40 -0500

    Add files via upload

--- PopulateAuthor.java
+++ PopulateAuthor.java
@@ -1,16 +1,20 @@
 package util;
 
+import java.util.HashMap;
+
 import business.Address;
 import business.Author;
+import business.LibraryMember;
 import dataaccess.DataAccessFacade;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 
 public class PopulateAuthor {
-	
-	private ObservableList<Author> personData = FXCollections.observableArrayList();
 	private DataAccessFacade dataaccess;
+	private ObservableList<Author> personData = FXCollections.observableArrayList();
+	private HashMap<String, Author> aut = new HashMap<>();
+	
 
 	public PopulateAuthor() {
 		// Add some sample data
@@ -18,10 +22,18 @@ public class PopulateAuthor {
 		ad.setCity("FF");
 		ad.setZip("262353");
 		ad.setState("IA");
-		Author a=new Author("Fasil","Habtegiorgis","2024646252",ad,"fasil is a student at mum")
+		Author a=new Author("Fasil","Habtegiorgis","2024646252",ad,"fasil is a student at mum");
+		for(Author temp:personData) {
+			String id=temp.getFirstName()+temp.getLastName();
+			aut.put(id, temp);
+			
+			
+		}
 		personData.add(a);
 		dataaccess=	new DataAccessFacade();
-		dataaccess.saveNewAuthor(a);
+		dataaccess.saveNewAuthor(aut);
 	}
+	
+	
 
 }
